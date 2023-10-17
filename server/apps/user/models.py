f339a7fc5dde0c1dd01001d37a6fcc8ee5e991c3
rush1dan <rushidan10@gmail.com>
2023-10-17 23:39:07 +0600

    User model created

--- server/apps/user/models.py
+++ server/apps/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+def user_image(instance, filename):
+    return f'user/{instance.id}/{filename}'
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False, null=False)
+    image = models.ImageField(upload_to=user_image, blank=False, null=False)
+    bio = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self) -> str:
+        return self.username
