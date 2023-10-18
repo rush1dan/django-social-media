c5550dff87f540aac9d5d0af8784dcdb9d08956c
rush1dan <rushidan10@gmail.com>
2023-10-18 09:49:44 +0600

    Comment model created

--- server/apps/comment/models.py
+++ server/apps/comment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from apps.post.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f'Comment by user {self.author.get_username} on {self.post} post'
+    
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
