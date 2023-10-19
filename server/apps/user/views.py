c1026aa2cb2b17479a58f286f90a3292e298b90e
rush1dan <rushidan10@gmail.com>
2023-10-20 00:30:23 +0600

    User login api endpoint implemented

--- server/apps/user/views.py
+++ server/apps/user/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from apps.user.serializers import (UserCreateSerializer)
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -28,19 +29,33 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 @api_view(['POST'])
 def register_view(request):
-    # Use this block if unique email needs to be enforced
-    # try:
-    #     user = User.objects.get(email=request.data['email'])
-    #     return Response("User Already Exists.", status=400)
-    # except ObjectDoesNotExist:
-    #     print("User doesn't exist...Good to go.")
-    
-    serializer = UserCreateSerializer(data=request.data)
-    if serializer.is_valid():   #Will be false for non-unique/null username
-        user = serializer.save()
-        return Response("User Created", status=200)
-    else:
-        return Response("Invalid Data", status=400)
+    try:
+        # Use this block if unique email needs to be enforced
+        # try:
+        #     user = User.objects.get(email=request.data['email'])
+        #     return Response("User Already Exists.", status=400)
+        # except ObjectDoesNotExist:
+        #     print("User doesn't exist...Good to go.")
+        
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():   #Will be false for non-unique/null username
+            user = serializer.save()
+            return Response("User Created", status=201)
+        else:
+            return Response("Invalid Data", status=400)
+    except Exception as ex:
+        return Response("Something went wrong", status=500)
     
 
     ## Authentications requires unique username and password only for default User model
+
+@api_view(['POST'])
+def login_view(request):
+    try:
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+        if user is not None:
+            return Response("User Authenticated", status=200)
+        else:
+            return Response("Invalid Credentials", status=401)
+    except Exception as ex:
+        return Response("Something went wrong", status=500)
