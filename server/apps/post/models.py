15cf18e2967431392dc344c736e438dc23c3c800
rush1dan <rushidan10@gmail.com>
2023-10-25 15:02:14 +0600

    Post Image upload path on backend modified

--- server/apps/post/models.py
+++ server/apps/post/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 def upload_to(instance, filename):
-    return f'post/{instance.id}/{filename}'
+    return f'user_{instance.author.id}/posts/{filename}'
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
