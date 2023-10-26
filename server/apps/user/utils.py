9a17f259e7aef448b8b0884d5936621ff4c3bfb6
rush1dan <rushidan10@gmail.com>
2023-10-26 18:30:30 +0600

    Minor refactor

--- server/apps/user/utils.py
+++ server/apps/user/utils.py
@@ -3,18 +3,18 @@ from .models import UserInfo
 from traceback import print_exc as print_error
 
 ## If requesting user is following target user
-def is_following(requesting_user, target_user):
+def is_following(requesting_user, target_user) -> bool:
     try:
-        return UserInfo.objects.filter(user=target_user, followers=requesting_user.info)
+        return UserInfo.objects.filter(user=target_user, followers=requesting_user.info) != None
     except Exception as ex:
         print_error()
         return False
     
 
 ## If requesting user is followedby target user
-def is_followedby(requesting_user, target_user):
+def is_followedby(requesting_user, target_user) -> bool:
     try:
-        return UserInfo.objects.filter(user=requesting_user, followers=target_user.info)
+        return UserInfo.objects.filter(user=requesting_user, followers=target_user.info) != None
     except Exception as ex:
         print_error()
         return False
