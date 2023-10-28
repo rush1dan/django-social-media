1e77e26ea5e767f6e8ed3047543bc8140ea4e4e2
rush1dan <rushidan10@gmail.com>
2023-10-28 18:51:21 +0600

    Popup slightly modified

--- client/components/Popup.tsx
+++ client/components/Popup.tsx
@@ -27,17 +27,15 @@ const Popup = ({ children, className, crossSizeClass, crossOffsetClass, hidden =
     }
 
     return (
-            <div className={`absolute top-0 left-0 w-full h-full bg-gray-900/25 z-10 ${closed ? 'hidden' : ''}`}>
-                <div className='relative w-full h-full'>
-                    <div className={className}>
-                        <button type='button' className={`absolute -translate-y-1/2 translate-x-1/2 ${crossSizeClass} ${crossOffsetClass}`}
-                            onClick={e => crossHandler?.()}>
-                            <Image loading='eager' src='/x.svg' alt='' fill />
-                        </button>
-                        {children}
-                    </div>
-                </div>
+        <div className={`absolute top-0 left-0 w-full h-full bg-gray-900/25 z-10 ${closed ? 'hidden' : ''}`}>
+            <div className={className}>
+                <button type='button' className={`absolute -translate-y-1/2 translate-x-1/2 ${crossSizeClass} ${crossOffsetClass}`}
+                    onClick={e => crossHandler?.()}>
+                    <Image loading='eager' src='/x.svg' alt='' fill />
+                </button>
+                {children}
             </div>
+        </div>
     )
 }
 
