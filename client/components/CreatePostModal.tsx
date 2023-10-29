232bf159b3f6dc33b227ec0044781e01c92b3367
rush1dan <rushidan10@gmail.com>
2023-10-29 16:14:41 +0600

    Create post modal with input expansion and image selection implemented

---
+++ client/components/CreatePostModal.tsx
@@ -0,0 +1,80 @@
+import { User } from '@/data/typedata'
+import React, { ReactEventHandler, useEffect, useRef, useState } from 'react'
+import Image from 'next/image'
+
+type Props = {
+    user?: User
+}
+
+const CreatePostModal = ({ user }: Props) => {
+    useEffect(() => {
+        const postArea = document.getElementById("create_post");
+
+        function autoResize() {
+            if (postArea) {
+                postArea.style.height = "auto";
+                postArea.style.height = (postArea.scrollHeight) + "px";
+            }
+        }
+
+        postArea?.addEventListener("input", autoResize);
+    }, [])
+
+    const [inputImage, setInputImage] = useState<File | null>(null);
+    const [inputImageSrc, setInputImageSrc] = useState<string | null>(null);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+
+    const handleImageSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const imageFile: File | null = e.target.files !== null ? e.target.files[0] : null;
+
+        if (imageFile) {
+            setInputImage(imageFile);
+            setInputImageSrc(URL.createObjectURL(imageFile));
+        }
+    }
+
+    return (
+        <div className='w-full h-full p-6 overflow-y-auto'>
+            <div className='w-full h-full'>
+                <form onSubmit={e => handleSubmit(e)} className='w-full min-h-full pb-16'>
+
+                    <div className='flex flex-row items-center justify-start gap-x-2 mb-2'>
+                        <div className='flex-none w-12 h-12 rounded-full relative bg-slate-500'>
+                            <Image src={user?.image ?? '/placeholder_image.svg'} alt='dp' fill />
+                        </div>
+                        <p className='font-semibold'>{user?.first_name} {user?.last_name}</p>
+                    </div>
+
+                    <textarea className='w-full mb-4 overflow-hidden rounded-lg bg-slate-100/10 resize-none text-base' name="create_post" id="create_post" rows={1}
+                        placeholder={`What's on your mind, ${user?.first_name}?`}>
+
+                    </textarea>
+
+                    <div className='w-full h-fit'>
+                        <label>
+                            <input type="file" accept="image/png, image/jpeg" className='hidden' onChange={e => handleImageSelection(e)} />
+                            {
+                                inputImageSrc ?
+                                    <div className='w-full aspect-square relative cursor-pointer'>
+                                        <Image src={inputImageSrc} alt='image' fill />
+                                    </div> :
+                                    <div className='w-full h-32 rounded-lg bg-slate-200 border-2 border-slate-300 flex flex-row items-center justify-center gap-x-2 cursor-pointer'>
+                                        <div className='w-12 h-12 relative'>
+                                            <Image src='/placeholder_image.svg' alt='image' fill />
+                                        </div>
+                                    </div>
+                            }
+                        </label>
+                    </div>
+
+                    <button type='submit' className='w-3/4 h-10 absolute bottom-4 left-1/2 -translate-x-1/2 bg-blue-500 hover:bg-blue-600 rounded-lg'>Post</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default CreatePostModal
--- client/components/HomePage/Feed.tsx
+++ client/components/HomePage/Feed.tsx
@@ -8,6 +8,8 @@ import { useAuth } from '@/hooks/userAuth'
 import { FeedItemDataType } from '@/data/typedata'
 import PostCard from './PostCard'
 import Image from 'next/image'
+import Popup from '../Popup'
+import CreatePostModal from '../CreatePostModal'
 
 type Props = {}
 
@@ -39,6 +41,9 @@ const Feed = (props: Props) => {
     useEffect(() => {
         fetchData();
     }, []);
+
+    const [postModalOpened, setPostModalOpened] = useState<boolean>(false);
+
     return (
         <div className='w-full h-full'>
             {/* Create Post */}
@@ -47,14 +52,25 @@ const Feed = (props: Props) => {
                     <div className='flex-none w-12 h-12 rounded-full relative bg-slate-500'>
                         <Image src={user?.image ?? '/placeholder_image.svg'} alt='dp' fill />
                     </div>
-                    <button type='button' className='w-full rounded-full h-10 text-left px-4 py-2 bg-slate-200 hover:bg-slate-300 text-gray-500 font-medium'>
+                    <button type='button' className='w-full rounded-full h-10 text-left px-4 py-2 bg-slate-200 hover:bg-slate-300 text-gray-500 font-medium'
+                        onClick={e => setPostModalOpened(true)}>
                         What's on your mind, {user?.first_name}?
                     </button>
                 </div>
-                <button type='button' className='w-full h-10 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md'>
+                <button type='button' className='w-full h-10 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md'
+                    onClick={e => setPostModalOpened(true)}>
                     Create Post
                 </button>
             </div>
+
+            {
+                postModalOpened &&
+                <Popup className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[28rem] h-[28rem] pt-16 rounded-lg bg-slate-50 overflow-clip'
+                        hidden={!postModalOpened} onClosed={() => setPostModalOpened(false)}
+                        popUpHeader='Create Post'>
+                        <CreatePostModal user={user} />
+                </Popup>
+            }
 
             {/* Post Cards */}
             <LoadingWrapper fetchState={fetchState} fetchInfo={fetchMsg}>
