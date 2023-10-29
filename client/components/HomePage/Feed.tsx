9b9fdf0914baecbc5edeb493d469dc16a03b1739
rush1dan <rushidan10@gmail.com>
2023-10-29 11:34:10 +0600

    Create post card on feed

--- client/components/HomePage/Feed.tsx
+++ client/components/HomePage/Feed.tsx
@@ -7,6 +7,7 @@ import axios from 'axios'
 import { useAuth } from '@/hooks/userAuth'
 import { FeedItemDataType } from '@/data/typedata'
 import PostCard from './PostCard'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -39,17 +40,35 @@ const Feed = (props: Props) => {
         fetchData();
     }, []);
     return (
-        <LoadingWrapper fetchState={fetchState} fetchInfo={fetchMsg}>
-            <div className='w-full min-h-full flex flex-col items-center justify-start gap-y-6'>
-                {
-                    feedData?.map((data, index) => {
-                    return (
-                        <PostCard feedItem={data} key={data.post.id} />
-                    )
-                    })
-                }
+        <div className='w-full h-full'>
+            {/* Create Post */}
+            <div className='w-full h-fit p-2 bg-slate-50 mb-6 rounded-lg'>
+                <div className='flex flex-row items-center justify-start gap-x-2 mb-2'>
+                    <div className='flex-none w-12 h-12 rounded-full relative bg-slate-500'>
+                        <Image src={user?.image ?? '/placeholder_image.svg'} alt='dp' fill />
+                    </div>
+                    <button type='button' className='w-full rounded-full h-10 text-left px-4 py-2 bg-slate-200 hover:bg-slate-300 text-gray-500 font-medium'>
+                        What's on your mind, {user?.first_name}?
+                    </button>
+                </div>
+                <button type='button' className='w-full h-10 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md'>
+                    Create Post
+                </button>
             </div>
-        </LoadingWrapper>
+
+            {/* Post Cards */}
+            <LoadingWrapper fetchState={fetchState} fetchInfo={fetchMsg}>
+                <div className='w-full min-h-full flex flex-col items-center justify-start gap-y-6'>
+                    {
+                        feedData?.map((data, index) => {
+                            return (
+                                <PostCard feedItem={data} key={data.post.id} />
+                            )
+                        })
+                    }
+                </div>
+            </LoadingWrapper>
+        </div>
     )
 }
 
